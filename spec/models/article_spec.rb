291046b2ff42431fe00deea985ba94b43265cf66
faraznaeem <faraznaeem@gmail.com>
2019-07-01 15:04:48 +0200

    Refactors basic steps, adds specs for factory and validates presence of attributes

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :content }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :content }
+  end
+
+  describe 'Factory' do
+    it 'has a valid factory' do
+      expect(FactoryBot.create(:article)).to be_valid
+    end
+  end
 end
